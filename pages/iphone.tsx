3bfe6f475c98c446a6bbcd16febcf7c19baf01fc
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2023-01-18 00:35:24 +0500

    deploy

--- pages/iphone.tsx
+++ pages/iphone.tsx
@@ -1,10 +1,17 @@
-import Navbar from "../components/Navbar";
-
-
+import Link from "next/link"
 export default function iphone() {
   return (
     <>
-    <Navbar/>
+    <div className='navbar'>
+      <Link href='#' className="logo">dp.uz</Link>
+      <div className="navs">
+        <Link href="/" className="nav-link">Store</Link>
+        <Link href="/iphone" className="nav-link">Iphone</Link>
+        <Link href="/mac" className="nav-link">Mac</Link>
+        <Link href="/ipad" className="nav-link">Ipad</Link>
+        <Link href="/accessories" className="nav-link">Accessories</Link>
+      </div>
+    </div>
     <div className="title_container">
         <h3>Iphones</h3>
       </div>
